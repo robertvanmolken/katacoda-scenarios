af402f7949fc227a2f2f0078c69bebb72c178c66
Robert van Mölken <rmulken@gmail.com>
2019-02-01 22:18:29 +0100

    Updated installation script

--- helidon-intro/install_helidon.sh
+++ helidon-intro/install_helidon.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-apt update 1> /dev/null 2> /dev/null \ 
-    && apt install openjdk-8-jdk -y 1> /dev/null 2> /dev/null \ 
-    && apt install maven -y 1> /dev/null 2> /dev/null \ 
-    && clear \ 
-    && echo "~~~Prerequisites installed successful~~~"
+apt update 1> /dev/null 2> /dev/null \
+&& apt install openjdk-8-jdk -y 1> /dev/null 2> /dev/null \
+&& apt install maven -y 1> /dev/null 2> /dev/null \
+&& clear \
+&& echo "~~~Prerequisites installed successful~~~"
