ef0d5ebbd801d6e639afb514a07e10c214da2958
Robert van Mölken <rmulken@gmail.com>
2019-02-01 20:35:42 +0100

    Fixed bug with prerequirements scripts

--- helidon-intro/install_helidon.sh
+++ helidon-intro/install_helidon.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo "~~~Installing Helidon prerequisites~~~"
-apt update && apt install openjdk-8-jdk -y && apt install maven -y
-
-clear
+echo "~~~ Installing Helidon prerequisites ~~~"
+apt update 1> /dev/null 2> /dev/null && apt install openjdk-8-jdk -y 1> /dev/null 2> /dev/null && apt install maven -y 1> /dev/null 2> /dev/null && clear
+echo "~~~Prerequisites installed successful~~~"
